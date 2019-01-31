ac59d7a93b58f12603b13e80168d7de706e325c6
AeroRogue <36310918+AeroRogue@users.noreply.github.com>
2019-01-31 14:39:52 -0500

    Working on more Bow and Arrow

---
+++ Operation Cobra/Assets/Scripts/ArrowManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowManager : MonoBehaviour {
+
+    //private OVRPlugin.p
+
+    private GameObject currentArrow;
+
+    public GameObject arrowPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /*public void AttachArrow()
+    {
+        if (currentArrow == null)
+        {
+            currentArrow = Instantiate(arrowPrefab);
+        }
+    }*/
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bow")
+        {
+            collision.transform.parent = transform;
+            collision.transform.localScale = transform.localScale;
+            collision.transform.localPosition = transform.localPosition;
+            // collision.rigidbody.detectCollisions = false;
+            //collision.gameObject.GetComponent<>().inHand = true;
+            collision.gameObject.GetComponent<Bow>().inHand = true;
+        }
+    }
+}
---
+++ Operation Cobra/Assets/Scripts/Bow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour {
+
+    public bool inHand;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (inHand == true && OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            this.transform.localRotation = GameObject.Find("Arrow").transform.localRotation;
+            this.transform.localPosition = GameObject.Find("Arrow").transform.localPosition;
+        }
+	}
+}

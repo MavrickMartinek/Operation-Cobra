cc996210525a603a47a01bbc674bf5f657b256d6
AeroRogue <cameron.krueger@outlook.com>
2019-02-27 01:26:42 -0500

    Finished most of the Fruit Ninja demo

--- Operation Cobra/Assets/Scripts/SwordCutter.cs
+++ Operation Cobra/Assets/Scripts/SwordCutter.cs
@@ -7,36 +7,6 @@ public class SwordCutter : MonoBehaviour {
 
     public Material capMaterial;
 
-    // Use this for initialization
-    void Start()
-    {
-
-
-    }
-
-    /*void Update()
-    {
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-
-            if (Physics.Raycast(transform.position, transform.forward, out hit))
-            {
-
-                GameObject victim = hit.collider.gameObject;
-
-                GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
-
-                if (!pieces[1].GetComponent<Rigidbody>())
-                    pieces[1].AddComponent<Rigidbody>();
-
-                Destroy(pieces[1], 1);
-            }
-
-        }
-    }*/
-
     void OnCollisionEnter(Collision collision)
     {
         GameObject victim = collision.collider.gameObject;
@@ -44,9 +14,13 @@ public class SwordCutter : MonoBehaviour {
         GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
 
         if (!pieces[1].GetComponent<Rigidbody>())
+        {
             pieces[1].AddComponent<Rigidbody>();
+            MeshCollider temp = pieces[1].AddComponent<MeshCollider>();
+            temp.convex = true;
+        }
 
-        Destroy(pieces[1], 1);
+        //Destroy(pieces[1], 1);
     }
 
 }

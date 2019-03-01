a9f8b156233414099d7e074d56a217bc88da538b
AeroRogue <36310918+AeroRogue@users.noreply.github.com>
2019-02-28 20:51:49 -0500

    Added objects with health (cylinders) to bow and sword scene and added damage to sword

---
+++ Operation Cobra/Assets/Scripts/SwordDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordDamage : MonoBehaviour {
+
+    public float damage;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "sword")
+        {
+            Health target = col.gameObject.GetComponent<Health>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}

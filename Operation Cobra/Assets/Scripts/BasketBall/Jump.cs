d8174efd6ed90679f23b25a93ec90e4d0485c65a
Mavrick <36382515+CanaMavrick@users.noreply.github.com>
2019-03-21 14:35:16 -0400

    Started work on jumping

---
+++ Operation Cobra/Assets/Scripts/BasketBall/Jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+    //https://forum.unity.com/threads/simple-way-to-have-smooth-jumping-without-rigid-body-finally.249488/
+    private bool rdyToJump;
+    // Use this for initialization
+    void Start () {
+		
+	}
+    private void FixedUpdate()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}

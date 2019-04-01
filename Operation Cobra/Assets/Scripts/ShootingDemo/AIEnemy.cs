25dee8e8e49083607990a104f5c8f05ecde28764
Mavrick <36382515+CanaMavrick@users.noreply.github.com>
2019-03-31 21:10:11 -0400

    Added moving target to bow/arrow (not working) + layed groundwork for ai

---
+++ Operation Cobra/Assets/Scripts/ShootingDemo/AIEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIEnemy : MonoBehaviour {
+    public bool alert;
+    public Camera AIVision;
+    public GameObject Player;
+    private Vector3 screenPoint;
+	// Use this for initialization
+	void Start () {
+        alert = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        screenPoint = Player.transform.position;
+        bool inFov = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+
+        if (inFov)
+        {
+            Debug.Log("Player Visible");
+            alert = true;
+        }
+        else
+        {
+            alert = false;
+        }
+
+	}
+}
